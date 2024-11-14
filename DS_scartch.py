42c77c8d744719c215db7c63fae545623b4c1312
0xdeadbeef <61603180+0xfacebad@users.noreply.github.com>
2024-11-13 20:12:29 -0800

    Update DS_scartch.py

--- DS_scartch.py
+++ DS_scartch.py
@@ -123,7 +123,7 @@ class DecisionTree:
                 return self._predict_sample(sample, node['right'])
         else:  # If we are at a leaf node
             return node
-
+#Choosing the small data sets 
 humidity_values = [6.1, 5.7, 4.6, 6, 4.4, 2.5, 2.6, 2.5, 4.9, 4.5, 6.5, 2.4, 2.5]
 wind_values = [4.5, 4.2, 2.5, 4.6, 1.2, 1.1, 1.2, 1.4, 1.4, 4.8, 2.3, 3.4, 2.6]
 playgolf_values = [0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1]
